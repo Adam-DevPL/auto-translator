a2fff967a2dd581e6a222fa8f7a793e60fdfe844
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2022-12-30 18:53:47 +0100

    feature: google translaotr api module

---
+++ src/Translator/Translator.ts
@@ -0,0 +1,25 @@
+import { Translate } from "@google-cloud/translate/build/src/v2/index";
+
+export class Translator {
+  readonly CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+  readonly translateConfig: Translate = new Translate({
+    credentials: this.CREDENTIALS,
+    projectId: this.CREDENTIALS.projectId,
+  });
+
+  public async translate(
+    text: string,
+    targetLanguage: string
+  ): Promise<string> {
+    try {
+      const [response] = await this.translateConfig.translate(
+        text,
+        targetLanguage
+      );
+      return response;
+    } catch (error) {
+      console.log(error);
+      return "";
+    }
+  }
+}
